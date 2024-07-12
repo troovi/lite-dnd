f8bec053a8701c72240696071b849a6954b404f7
troovi <troovi@icloud.com>
2024-07-12 12:32:23 +0800

    update

--- make-package.js
+++ make-package.js
@@ -6,5 +6,8 @@ delete rootPackage.scripts
 delete rootPackage.workspaces
 
 rootPackage.dependencies = dndPackage.dependencies
+rootPackage.main = dndPackage.main.replace('dist/', '')
+rootPackage.module = dndPackage.module.replace('dist/', '')
+rootPackage.types = dndPackage.types.replace('dist/', '')
 
-writeFileSync('./dnd/dist/package.json', JSON.stringify(rootPackage))
+writeFileSync('./dnd/dist/package.json', JSON.stringify(rootPackage, null, 2))
